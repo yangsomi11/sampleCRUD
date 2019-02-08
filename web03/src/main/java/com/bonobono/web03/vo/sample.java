db27bded4c9d9fbc9f34a9e2e74b8c3f8ab8cca8
Administrator <Administrator@smart000>
2019-02-08 10:56:15 +0900

    [양소미]sample 생성 -sample 테이블 vo 생성

--- web03/src/main/java/com/bonobono/web03/vo/sample.java
+++ web03/src/main/java/com/bonobono/web03/vo/sample.java
@@ -1,5 +1,25 @@
 package com.bonobono.web03.vo;
 
-public class sample {
 
+public class sample {
+	private int sampleId;//sample.sample_id (클레스안에 컬럼)
+	private String sampleName; //sample클레스내에  sample_name컬럼
+	
+	public int getSampleId() {
+		return sampleId;
+	}
+	public void setSampleId(int sampleId) {
+		this.sampleId = sampleId;
+	}
+	public String getSampleName() {
+		return sampleName;
+	}
+	public void setSampleName(String sampleName) {
+		this.sampleName = sampleName;
+	}
+	@Override
+	public String toString() {
+		return "sample [sampleId=" + sampleId + ", sampleName=" + sampleName + "]";
+	}
+	
 }
