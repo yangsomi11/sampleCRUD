4e86bfdfead49542e2fa34bf95be28ca895f0a44
Administrator <Administrator@smart000>
2019-02-13 15:55:01 +0900

    [양소미] SampleController수정 SampleMapper.xml 주석
    - removeSample 메서드에서 int type으로 수정
    - SampleMapper.xml에 쿼리설명 주석추가

--- web03/src/main/java/com/bonobono/web03/mapper/SampleMapper.java
+++ web03/src/main/java/com/bonobono/web03/mapper/SampleMapper.java
@@ -8,7 +8,13 @@ import com.bonobono.web03.vo.Sample;
 
 @Mapper
 public interface SampleMapper {
+	//목록
 	List<Sample> selectsampleList();
+	//추가
 	int insertSample(String sampleName);
+	//삭제
+	int deleteSample(int sampleId);
+	
+		
 }
 
